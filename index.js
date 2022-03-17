f698ae699b2116f4d1d90cd3d81b4a1b2aac5533
Amber Frantz <amber.k.frantz@mgail.com>
2022-03-17 15:21:14 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solutions in this file
+
+function countdown(num){
+    while(num >= 0){
+        console.log(num)
+        num--
+    }
+}
+
+
+
+
+
+
+
+
+
+
+/*const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+const names = ["Amber", "Scott", "Eevee"]
+
+function writeCards(names, event){
+    for(let i = 0; i < names.length; i++){
+        console.log(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+}
+
+writeCards(names,"birthday")
+*/
